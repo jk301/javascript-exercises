727847626a8eaa5e11b28375a9e6b95ef303fc44
junaid <jk7948677@gmail.com>
2026-03-01 18:57:34 +0530

    foundations 07-11 done

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...num) {
+    for (let i = 0; i < array.length; i++) {
+        for (let item of num) {
+            if (array.includes(item)) {
+                array.splice(array.indexOf(item), 1);
+            }
+        }
+    }
+    return array;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(initial, end) {
+    if (Number.isInteger(initial) === false || initial < 0 || Number.isInteger(end) === false || end < 0) {
+        return "ERROR"
+    } else {
+        let sum = 0;
+        for (let i = Math.min(initial, end); i <= Math.max(initial, end); i++) {
+            sum += i ;
+        }
+        return sum
+    }
 };
-
 // Do not edit below this line
 module.exports = sumAll;
