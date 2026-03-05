eddee915f396270a4a685885048f8bf60c4c2696
junaid <jk7948677@gmail.com>
2026-03-05 23:09:38 +0530

    foundations done from 12-14

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  return a.reduce((sum = 0, num) => sum += num, 0)	
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  return a.reduce((sum , num) => sum *= num, 1)	;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let result = 1;
+  if (a === 0 || a === 1) {
+    return 1;
+  } else {
+    for (let i = 1; i <= a; i++) {
+      result *= i;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    plain = string.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+    const reversed = plain.split("").reverse().join("");
+    return plain === reversed ? true : false;
 };
 
 // Do not edit below this line
