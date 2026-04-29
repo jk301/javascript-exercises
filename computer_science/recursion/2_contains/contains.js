83a1f954412e896bee90c0e52930f6138ce9e304
junaid <jk7948677@gmail.com>
2026-04-30 00:20:25 +0530

    Did contains

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,5 +1,13 @@
-const contains = function() {
-  
+const contains = function(object, find) {
+  for (let [key, value] of Object.entries(object)) {
+    if (value === find || Number.isNaN(value) && Number.isNaN(find)) {
+      return true
+    } else if (typeof value === "object" && value !== null) {
+      const result = contains(value, find)
+      if (result === true) return true;
+    }
+  }
+  return false
 };
   
 // Do not edit below this line
