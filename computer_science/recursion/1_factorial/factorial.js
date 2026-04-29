fd539642689f1a2898f67df2ffedeca2f584ea40
junaid <jk7948677@gmail.com>
2026-04-29 21:29:34 +0530

    Did factorial

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,11 @@
-const factorial = function() {
-
+const factorial = function(num) {
+    if (num < 0 || typeof num !== "number" || num % 1 !== 0) {
+        return 
+    } else if (num === 1 || num === 0) {
+        return 1
+    } else {
+        return num * factorial(num - 1)
+    }
 };
 
 // Do not edit below this line
