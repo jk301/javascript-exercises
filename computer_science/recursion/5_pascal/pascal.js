1e8981f60d830a6ca6fa4e9dab249324f400cf48
junaid <jk7948677@gmail.com>
2026-05-01 01:22:00 +0530

    Did pascal (gah damn again)

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,6 +1,16 @@
-const pascal = function() {
-  
+const pascal = function(step, arr = [1]) {
+  if (step === 1) return arr;
+  const looper = [0, ...arr, 0]
+  const newe = []
+  for (let i = 0; i < looper.length; i++) {
+    newe.push(looper[i] + looper[i + 1])
+  }
+  // extra one was popping up because of [i + 1]
+  newe.pop()
+  const result = pascal(step - 1, newe)
+  return result
 };
-  
+
+
 // Do not edit below this line
 module.exports = pascal;
