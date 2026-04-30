278338f27a295c24dd3c2bf3eb91598fd48654e7
junaid <jk7948677@gmail.com>
2026-04-30 12:52:52 +0530

    Did totalIntegers

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,19 @@
-const totalIntegers = function() {
-  
-};
+const totalIntegers = function(object) {
+    if (typeof object !== "object") return
+    let counter = 0
+    const array = Object.values(object)
+    array.forEach (item => {
+        if(Number.isInteger(item)){
+            counter++
+            console.log("it is " + `counter : ${counter}`)
+        } else if (typeof item === "object" && item !== null) {
+            const val = totalIntegers(item)
+            counter += val
+            console.log("it is not " + `counter : ${counter}`)
+        }
+    })
+    return counter
+}
   
 // Do not edit below this line
 module.exports = totalIntegers;
