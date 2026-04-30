5948baa60ebd583fd8a2f7b725f622c2634a1f75
junaid <jk7948677@gmail.com>
2026-04-30 18:31:21 +0530

    Did permutations gahdam!

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,6 +1,18 @@
-const permutations = function() {
-  
-};
+const permutations =  function (array) {
+    const result = []
+    if (array.length <= 1) {
+        return [array]
+    };
+    array.forEach( item => {
+        const newArray = array.filter(el => el !== item)
+        const mutate = permutations(newArray)
+        mutate.forEach(per => {
+            result.push([item, ...per])
+        })
+    })
+    return result
+}
+
   
 // Do not edit below this line
 module.exports = permutations;
